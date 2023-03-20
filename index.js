1566e59009af317a38fd3add723fb81e5ce8d91a
Aodhan <hamilton.aodhan@gmail.com>
2023-03-20 22:11:04 +1300

    just a fun webhook with some friends

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const {
 	GatewayIntentBits,
 	WebhookClient,
 } = require("discord.js");
+const CronJob = require("cron").CronJob;
 
 const webhookClient = new WebhookClient({
 	url: process.env.DISCORD_WEBHOOK_URL,
@@ -18,10 +19,19 @@ client.once(Events.ClientReady, (c) => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-const sendWebhook = () => {
-	webhookClient.send("<@354095834432012300>");
-};
+// const sendWebhook = () => {
+// 	webhookClient.send("Hi from the cron job");
+// };
+var job = new CronJob(
+	"0 11 11 * * *",
+	function () {
+		webhookClient.send("It's 11:11 bozo, make a wish <@1032480393540153364> ");
+	},
+	null,
+	true,
+	"America/New_York"
+);
+job.start();
 
-sendWebhook();
 // Log in to Discord with your client's token
 client.login(process.env.DISCORD_TOKEN);
